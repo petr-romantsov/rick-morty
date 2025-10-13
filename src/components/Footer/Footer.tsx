9831ca8d7ff608f51ef115ea312a04a6feeeca1f
Petr Romantsov <romantsovp@gmail.com>
2025-10-13 12:58:12 +0300

    фикс: удалил лишний див в футере

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,9 +3,7 @@ import './Footer.css';
 export const Footer = () => {
   return (
     <footer className='footer'>
-      <div className='container'>
-        <p className='footer__text'>Made with love by @romantsoff</p>
-      </div>
+      <p className='footer__text'>Made with love by @romantsoff</p>
     </footer>
   );
 };
