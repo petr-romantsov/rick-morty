ebb643fd5dee3dcef4b7bebc452561b93be43376
Petr Romantsov <romantsovp@gmail.com>
2026-03-24 23:13:39 +0300

    заменил context на zustand

---
+++ src/stores/useFiltersStore.tsx
@@ -0,0 +1,51 @@
+import { memo } from 'react';
+
+import { type StateCreator, create } from 'zustand';
+import { useShallow } from 'zustand/react/shallow';
+
+import type { TFilters } from '@/shared/types';
+
+const initialFilters: TFilters = {
+  name: '',
+  species: '',
+  gender: '',
+  status: null
+};
+
+let nameDebounceTimer: ReturnType<typeof setTimeout> | null = null;
+const NAME_DEBOUNCE_DELAY = 1000;
+
+type TFiltersStoreState = {
+  filters: TFilters;
+  nameInputValue: string;
+};
+
+type TFiltersStoreActions = {
+  setFilters: (filter: keyof TFilters, value: string | null) => void;
+  setNameInputValue: (value: string) => void;
+};
+
+type TFiltersStore = TFiltersStoreState & TFiltersStoreActions;
+
+const filtersStore: StateCreator<TFiltersStore> = (set) => ({
+  filters: initialFilters,
+  nameInputValue: '',
+
+  setFilters: (filter, value) =>
+    set((state) => ({ filters: { ...state.filters, [filter]: value } })),
+
+  setNameInputValue: (value) => {
+    set({ nameInputValue: value });
+
+    if (nameDebounceTimer !== null) clearTimeout(nameDebounceTimer);
+
+    nameDebounceTimer = setTimeout(() => {
+      set((state) => ({ filters: { ...state.filters, name: value } }));
+      nameDebounceTimer = null;
+    }, NAME_DEBOUNCE_DELAY);
+  }
+});
+
+export const useFiltersStore = create<TFiltersStore>(filtersStore);
+
+export const useFilters = () => useFiltersStore((state) => state.filters);
--- src/widgets/FilterPanel/FilterPanel.tsx
+++ src/widgets/FilterPanel/FilterPanel.tsx
@@ -1,5 +1,7 @@
 import { type FormEventHandler, memo, useContext } from 'react';
 
+import { useShallow } from 'zustand/react/shallow';
+
 import { SearchIcon } from '@/assets';
 import {
   GENDER_OPTIONS,
@@ -8,7 +10,7 @@ import {
   STATUS_OPTIONS,
   Select
 } from '@/shared/components';
-import { CharactersFiltersContext } from '@/stores';
+import { useFiltersStore } from '@/stores';
 
 import './FilterPanel.scss';
 
@@ -19,8 +21,15 @@ const selects = [
 ] as const;
 
 export const FilterPanel = memo(() => {
-  const { nameInputValue, filters, handleFilterChange, handleNameChange } =
-    useContext(CharactersFiltersContext);
+  const { filters, nameInputValue, handleFilterChange, handleNameChange } =
+    useFiltersStore(
+      useShallow((state) => ({
+        filters: state.filters,
+        nameInputValue: state.nameInputValue,
+        handleFilterChange: state.setFilters,
+        handleNameChange: state.setNameInputValue
+      }))
+    );
 
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => e.preventDefault();
 
